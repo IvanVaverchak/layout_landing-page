4726bba16e318deea3790c6fba44a12d77047264
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-12-04 10:30:52 +0200

    make build

---
+++ index.f67deb32.js
@@ -0,0 +1,3 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector(".form").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});//# sourceMappingURL=index.f67deb32.js.map
+
+//# sourceMappingURL=index.f67deb32.js.map
